679b02054ca11d68182cb69faecb1a306b6faaa7
k-law21 <kwame@udel.edu>
2020-01-01 19:49:37 -0500

    TDD for Person class is done

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,31 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Test;
+import org.junit.Assert;
 public class TestPerson {
+
+
+    @Test
+
+    public void  testConstructor(){
+        Person person = new Person(10L,"Kwame");
+        String expected = person.getName();
+        String  actual = "Kwame";
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+
+    public void   testSetName () {
+        Person person = new Person(10L,"NewName");
+        String expectedName = "New";
+        person.setName(expectedName);
+
+        String actualName = person.getName();
+
+        Assert.assertEquals(expectedName, actualName);
+
+    }
 
 }
